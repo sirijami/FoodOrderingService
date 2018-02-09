34bea4dd97b0e03b5497a7f949a665e2e8c92624
sirijami <sirishaepari@192.168.0.100>
2018-02-08 23:28:06 -0800

    Dynamodb Local database initial commit

--- src/main/java/FoodOrderingServicePackage/FoodOrderingService/database/DatabaseClass.java
+++ src/main/java/FoodOrderingServicePackage/FoodOrderingService/database/DatabaseClass.java
@@ -1,8 +1,19 @@
 package FoodOrderingServicePackage.FoodOrderingService.database;
 
-import java.awt.MenuItem;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
 
 import FoodOrderingServicePackage.FoodOrderingService.model.Menu;
 import FoodOrderingServicePackage.FoodOrderingService.model.MenuItems;
@@ -13,7 +24,8 @@ public class DatabaseClass {
 	private static Map<String,Restaurant> restaurants = new HashMap<String,Restaurant>();
 	private static Map<Long,MenuItems> menuItems = new HashMap<Long, MenuItems>();
 	private static Map<Long,Menu> menus = new HashMap<Long,Menu>();
-	
+	private static AmazonDynamoDB db = DynamoDBEmbedded.create().amazonDynamoDB();
+
 	
 	public static Map<String, Restaurant> getRestaurants(){
 		return restaurants;
@@ -27,6 +39,22 @@ public class DatabaseClass {
 		return menus;
 	}
 	
-	
+	private static CreateTableResult createResturants(String tableName, String hashKeyName) {
+        List<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+        attributeDefinitions.add(new AttributeDefinition(hashKeyName, ScalarAttributeType.N));
 
+        List<KeySchemaElement> ks = new ArrayList<KeySchemaElement>();
+        ks.add(new KeySchemaElement(hashKeyName, KeyType.HASH));
+
+        ProvisionedThroughput provisionedthroughput = new ProvisionedThroughput(1000L, 1000L);
+
+        CreateTableRequest request =
+            new CreateTableRequest()
+                .withTableName(tableName)
+                .withAttributeDefinitions(attributeDefinitions)
+                .withKeySchema(ks)
+                .withProvisionedThroughput(provisionedthroughput);
+
+        return db.createTable(request);
+	}
 }
